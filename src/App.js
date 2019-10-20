50c0dcb66b8b982ca3e5b45fabb63c5af3e7a66e
kimberlybone <k_bone@uncg.edu>
2019-10-20 18:51:24 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,43 @@
 // create your App component here
+import React from 'react'
+
+class App extends React.Component {
+  state = {
+    peopleInSpace: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(peopleInSpace => {
+      let allInSpace = peopleInSpace.people
+      this.setState({
+        peopleInSpace: [...allInSpace]
+      })
+    })
+  }
+  getEachAstro = () => {
+    return this.state.peopleInSpace.map(person => {
+      console.log(person.name)
+      return person.name
+    })
+  }
+
+  render() {
+    // console.log(this.state.peopleInSpace)
+    return (
+      <div>
+      <ul>
+      {
+        this.state.peopleInSpace.map(person => {
+          console.log(person.name)
+          return person.name
+        })
+        // this.getEachAstro()
+      }
+    </ul>
+     </div>
+  )
+  }
+}
+export default App
